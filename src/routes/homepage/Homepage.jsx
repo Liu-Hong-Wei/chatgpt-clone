87885ffb472f676abe2b3db4514c3ce0d2156395
Homey Lau <lhw1953927853@gmail.com>
2024-11-20 00:05:37 +0800

    feat: homepage basic structure

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -2,7 +2,30 @@ import { Link } from "react-router-dom";
 import "./homepage.css";
 
 const Homepage = () => {
-  return <div className="Homepage"><Link to="./dashboard">Dashboard</Link></div>;
+  return (
+    <div className="homepage">
+      <img src="/orbital.png" alt="" className="orbital"/>
+      <div className="left">
+        <h1>ChatGPT Clone</h1>
+        <h2>This Project is aimed to learn React.</h2>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+          expedita quidem sunt distinctio aliquid. Sed atque deleniti odio eum?
+          Animi ullam maiores illum dolore earum possimus. Eius est error illo?
+        </h3>
+        {/* <button>Get Started</button> */}
+        <Link to="./dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="./bot.png" alt="" className="bot"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
