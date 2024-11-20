c2bfeb5fdbdf5e80fcad84cf1a7c2637d94557b8
Homey Lau <lhw1953927853@gmail.com>
2024-11-20 11:27:18 +0800

    feat: homepage chat image and responsive design

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [typeStatus, setTypeStatus] = useState("human1");
+
   return (
     <div className="homepage">
-      <img src="/orbital.png" alt="" className="orbital"/>
+      <img src="/orbital.png" alt="" className="orbital" />
       <div className="left">
         <h1>ChatGPT Clone</h1>
         <h2>This Project is aimed to learn React.</h2>
@@ -21,7 +25,59 @@ const Homepage = () => {
           <div className="bgContainer">
             <div className="bg"></div>
           </div>
-          <img src="./bot.png" alt="" className="bot"/>
+          <img src="./bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typeStatus === "human1"
+                  ? "./human1.jpeg"
+                  : typeStatus === "human2"
+                  ? "./human2.jpeg"
+                  : "./bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "h: We produce food for Mice",
+                2000,
+                () => {
+                  setTypeStatus("human1");
+                },
+                "b: We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypeStatus("bot");
+                },
+                "h: We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypeStatus("human2");
+                },
+                "b: We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypeStatus("bot");
+                },
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <Link to="/">
+          <img src="/logo.png" alt="" />
+        </Link>
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
