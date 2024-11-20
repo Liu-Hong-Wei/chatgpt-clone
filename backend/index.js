432910f54916a2fd17c8cdf652d431400200ef50
Homey Lau <lhw1953927853@gmail.com>
2024-11-20 18:33:56 +0800

    refactor: create backend module

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const ImageKit = require("imagekit");
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+app.get("/api/upload", (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
