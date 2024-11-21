f98d08f611d7c0b724ebbc9141e7e1182b616398
Homey Lau <lhw1953927853@gmail.com>
2024-11-21 17:12:25 +0800

    feat: implement chat with model with Gemini API

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -2,19 +2,73 @@ import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   const [img, setImg] = useState({
     isLoading: false,
     error: "",
     dbData: {},
+    aiData: {},
+  });
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      // maxOutputTokens: 100,
+    },
   });
 
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  });
+  }, [question, answer, img]);
+
+  const add = async (prompt) => {
+    setQuestion(prompt);
+
+    const result = await chat.sendMessageStream(
+      Object.entries(img.aiData).length ? [img.aiData, prompt] : [prompt]
+    );
+
+    let accumulatedText = "";
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      accumulatedText += chunkText;
+      setAnswer(accumulatedText);
+    }
+
+    setImg({
+      isLoading: false,
+      error: "",
+      dbData: {},
+      aiData: {},
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const prompt = e.target.prompt.value;
+    e.target.prompt.value = "";
+    if (!prompt) {
+      return;
+    }
+    add(prompt);
+  };
 
   return (
     <>
@@ -27,15 +81,26 @@ const NewPrompt = () => {
           transformation={[{ width: 380 }]}
         />
       )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div ref={endRef}></div>
       <div className="endChat"></div>
-      <form className="newForm">
+      <form className="newForm" onSubmit={handleSubmit}>
         {/* <label htmlFor="file">
           <img src="/attachment.png" alt="" />
         </label> */}
         <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
-        <input id="prompt" type="text" placeholder="Message ChatGPT" />
+        <input
+          id="prompt"
+          name="prompt"
+          type="text"
+          placeholder="Message ChatGPT"
+        />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
